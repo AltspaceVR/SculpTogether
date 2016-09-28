71447e344b1060a61d1fbe0741b26266683f00a2
omgitsraven <omgitsraven@gmail.com>
2016-09-28 15:14:57 -0400

    - removed timer system - migrated reset system into shared.js since it was redundant where it was anyway

--- deploy/shared.js
+++ deploy/shared.js
@@ -43,3 +43,77 @@ function detectControlSystemMode(){
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+var previousWipeNum;
+
+function setupResetSystem(isPopup){
+	
+	var wipeInfoServer = artServer.child('wipeInfo');
+	wipeInfoServer.on('value',function(snapshot){
+		
+		var newWipeInfo = snapshot.val();
+		if (!newWipeInfo) console.log("no existing wipe info, this seems to be a new room!");
+		
+		var newWipeNum = newWipeInfo ? newWipeInfo.num : 1 ;
+		console.log("current wipenum val:",newWipeNum);
+		
+		if (previousWipeNum) {
+			
+			console.log("previous wipenum val:",previousWipeNum);
+			if (newWipeNum != previousWipeNum) {
+				console.log("they're different, so time to reload.");
+				if (walkingDead) {
+					console.log("EXCEPT, I'm the one who sent the message, so I'm not going to until I hear back from the server!!");
+				} else {
+					location.reload();
+				}
+			} else {
+				console.log("they still match, so no reloading just yet.");
+			}
+			
+		} else {
+			
+			console.log("this is the first wipenum I've seen, so I'll remember it.");
+			previousWipeNum = newWipeNum;
+			
+			if (isPopup){
+				var resetMsg = newWipeInfo ? ("Last reset was by "+newWipeInfo.user+".") : "This room has not yet been reset.";
+				document.getElementById('previousResetInfo').innerHTML = resetMsg;
+			}
+			
+		}
+		
+	});
+	
+	
+	
+}
+
+
+function resetRoom(){
+	
+	walkingDead = true;
+	
+	artServer.set({wipeInfo:{
+		num:(previousWipeNum+1),
+		user:userInfo.displayName
+	}},function(error){
+		console.log("ok, news of reset has reached server, so I can reset now.");
+		location.reload();
+	});
+	
+}
+
+
+
